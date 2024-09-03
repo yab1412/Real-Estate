d828c1b7898e96cb47e790b40b9a0cb950c5d368
yab1412 <yabmuluneh22@gmail.com>
2024-09-03 11:07:37 +0300

    aA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ProgressBar />
+        {/* <ProgressBar /> */}
         <header>
           <Header
             menu={[
